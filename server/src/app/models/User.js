cb9dac009185f00e8c37ff353549548b670593ba
andrei <andreixhz123@gmail.com>
2020-06-19 18:34:29 -0300

    Add User Model

---
+++ server/src/app/models/User.js
@@ -0,0 +1,57 @@
+const conn = require('../../database/database');
+const { Model , Sequelize, DataTypes } = require('sequelize');
+
+class User extends Model {}
+
+User.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    nome:{
+        type: DataTypes.STRING(20),
+        allowNull: false
+    },
+    sobrenome:{
+        type: DataTypes.STRING(20),
+        allowNull: false
+    },
+    sexo:{
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    nascimento:{
+        type: DataTypes.DATEONLY,
+        allowNull: false
+    }, 
+    cpf:{
+        type: DataTypes.STRING(11),
+        allowNull: false
+    },
+    telefone:{
+        type: DataTypes.STRING(20),
+        allowNull: true
+    },
+    celular:{
+        type: DataTypes.STRING(20),
+        allowNull: false
+    },
+    email:{
+        type: DataTypes.STRING(60),
+        allowNull: false
+    },
+    endereco:{
+        type: DataTypes.STRING(100),
+        allowNull: false
+    },
+
+    createdAt: Sequelize.DATE,
+    updatedAt: Sequelize.DATE,
+
+}, {
+    conn,
+    modelName: "user"
+})
+
+module.exports = User;
