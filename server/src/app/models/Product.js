96fadd1a0c27aef87baacd00a32e3338ab5f6379
andrei <andreixhz123@gmail.com>
2020-06-20 15:56:54 -0300

    Product Model

---
+++ server/src/app/models/Product.js
@@ -0,0 +1,41 @@
+const conn = require('../../database/database');
+const { Model , Sequelize, DataTypes } = require('sequelize');
+
+class Product extends Model {}
+
+Product.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    code_bar:{
+        type: DataTypes.STRING(20),
+        allowNull: true
+    },
+    name:{
+        type: DataTypes.STRING(50),
+        allowNull: true
+    },
+    description:{
+        type: DataTypes.STRING(50),
+        allowNull: true
+    },
+    brandId:{
+        type: DataTypes.INTEGER,
+        allowNull: true
+    },
+    image:{
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    createdAt: Sequelize.DATE,
+    updatedAt: Sequelize.DATE,
+
+}, {
+    freezeTableName: true,
+    sequelize: conn,
+    modelName: "product"
+})
+
+module.exports = Product;
