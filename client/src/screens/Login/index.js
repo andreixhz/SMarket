90aab2835aa0c9e61f69bece1eb753d7008158b2
andrei <andreixhz123@gmail.com>
2020-07-27 11:40:44 -0300

    Login screen

--- client/src/screens/Login/index.js
+++ client/src/screens/Login/index.js
@@ -1,12 +1,35 @@
 import React from 'react';
+import {Box, TextField,Button} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import styles from './Style';
 
-import './style.css';
+const useStyles = makeStyles((theme) => ({
+	inputText: {
+		marginButtom: '25px',
+		width: '300px',
+		height: '70px'
+	},
+
+	logo:{
+		marginBottom:'72px'
+	}
+
+}));
+  
 
 function Login() {
+	const classes = useStyles();
 	return (
 		<div className="d-flex d-all-center">
-			<form className="login-form">
-				<img src="http://localhost:3325/cfg/logo"/>
+			<form>
+				<Box alignItems="center" display="flex" flexDirection="column">
+					<img src="http://localhost:3325/cfg/logo" height="77" width="180" className={classes.logo}/>
+					<TextField id="standard-basic" label="Usuario" variant="outlined" className={classes.inputText} />
+					<TextField id="standard-basic" label="Senha" variant="outlined" className={classes.inputText} />
+					<Button variant="contained" color="#f1f1f1">
+						Primary
+					</Button>
+				</Box>
 			</form>
 		</div>
 	);
---
+++ client/src/screens/Login/style.js
@@ -0,0 +1,10 @@
+const style = {
+    loginForm:{
+        display: 'flex',
+        flexDirection: 'column',
+        width: 250
+    },
+    inputText:{
+        width:250
+    }
+}
