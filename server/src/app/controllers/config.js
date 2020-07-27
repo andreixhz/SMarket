f0496ab1eef037acd3e24d0b9a8edefde3af3e99
andrei <andreixhz123@gmail.com>
2020-07-27 11:43:23 -0300

    sending server config

--- server/src/app/controllers/config.js
+++ server/src/app/controllers/config.js
@@ -6,5 +6,17 @@ router.get('/logo', async(req, res) => {
     return res.sendFile(path.dirname(require.main.filename || process.mainModule.filename) + '\\cdn\\Logo.png');
 })
 
+router.get('/cfg', async(req,res) => {
+
+    const config = {
+        name:"Inveted Emprise",
+        color: "7AB949",
+        lang: "en_US"
+    }
+
+    return res.send(config);
+
+})
+
 
 module.exports = app => app.use('/cfg', router);
